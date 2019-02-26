19dfe3e6a2ad23dd9882a526907afe4984044fa0
gabrieldua95 <48035133+gabrieldua95@users.noreply.github.com>
2019-02-26 20:08:56 -0300

    Add files via upload

---
+++ aula26_02_ex2.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstdlib>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+// programa lê e imprime um valor
+int main(void)
+{
+	system("cls");
+	int a = 0;
+	scanf("%i",&a);
+	printf("O valor de A eh; %i \n",a);
+	system("pause");
+	return 0;
+}
