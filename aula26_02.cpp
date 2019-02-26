331edab42868c29c4979d94b9e68a995d714509b
gabrieldua95 <48035133+gabrieldua95@users.noreply.github.com>
2019-02-26 19:57:29 -0300

    Add files via upload

---
+++ aula26_02.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdlib>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+int main(void)
+{
+
+	int a=5,b=4;
+	
+	if (a>b)
+	{
+		printf("A e maior que B \n");
+	}
+	else
+	{
+		if(a<b)
+		{
+		printf("A e menor que B \n");
+		}
+		else
+		{
+		printf("A e igual B");
+		}
+	}
+	return 1;
+}
